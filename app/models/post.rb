39444ae751bdbf35a40ab2de029d5516b3e2ebd0
Samo <samo.zeleznik5@gmail.com>
2019-11-05 11:42:46 +0100

    Fix validation, fields and dependents

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :ime, :opis
+  validates :ime, length: { maximum: 25 }
+  validates :opis, length: { minimum: 50, message: 'Prosim vnesite daljši opis' }
 end
